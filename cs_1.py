901c7dfcdeee6fba6fcb392cb4198571529b4339
Rabab-Okasha <rababokasha123@gmail.com>
2023-12-28 17:43:52 +0200

    out-subtraction

--- cs_1.py
+++ cs_1.py
@@ -1,95 +1,122 @@
- #name:Rabab Mohamed Abd El-Gafour Okasha ID:20237004
+#name:Rabab Mohamed Abd El-Gafour Okasha ID:20237004
 #name:Basmalah Nabil El-Said Muhammed ID:20235005
  
- import re
- 
+import re
+# menu 1
 def display_menu_1():
     print("Menu 1:")
     print("A. Insert a new number")
     print("B. Exist")
     
-def get_user_choice_1():
-     input(choice_1)
+# menu 2
+def display_menu_2():
+    print("Please select the operation:")
+    print("A. Compute one's complement")
+    print("B. Compute two's complement")
+    print("C. addition")
+    print("D. subtraction\n")
+    
+def get_user_choice():
+     choice_1 = input("Please Enter Your Choice\n ")
      return choice_1
     
-# understand it well
-def validate_binary_number(num_1):
-    num_1 = input()
+# check a number is binary 
+def validate_binary_number(number):
     pattern = r'^[01]+$'  # Regular expression pattern for binary numbers
-    if re.match(pattern, num_1):
+    if re.match(pattern, number):
         return True
     else:
         return False
 
-def display_menu_2():
-    print("Please select the operation")
-    print("A. Compute one's complement")
-    print("B. Compute two's complement")
-    print("C. addition")
-    print("D. subtraction")
-    
-def get_user_choice_2():
-     input(choice_2)
-     return choice_2
-    
-#def one_complement():
-    
-#def two_complement():
-    
-def addition():
-    #understand it well
-    if validate_binary_number():
-       print("Valid binary number.")
-    else:
-       print("Please insert a valid binary number.")
-    num_2 = input("Enter the second number")
-    sum = num_1 + num_2
-    return sum
+def insert_number():
+     num_2 = input("Please enter second  number \n")
+     while not validate_binary_number(num_2):
+        print("Please insert a valid binary number\n")
+        num_2 = input("Please enter second number again\n")
+     return num_2
 
-def subtraction():
-    #understand it well
-    if validate_binary_number(num_1):
-       print("Valid binary number.")
+def bin_add(num_1,num_2):
+        num_1=str(num_1)
+        num_2=str(num_2)
+        result = ""
+        carry = 0
+        i, j = len(num_1) - 1, len(num_2) - 1
+        while i >= 0 or j >= 0 or carry != 0:
+            sum = carry
+            if i >= 0:
+                sum += int(num_1[i])
+                i -= 1
+            if j >= 0:
+                sum += int(num_2[j])
+                j -= 1
+            carry = sum // 2
+            result = str(sum % 2) + result
+        return result 
+def one_complement(number):
+    result = ""
+    for i in number:
+        if i == '0':
+            result += "1"
+        else: 
+            result += "0"
+    return result
+    
+def two_complement(num_1):
+    result = one_complement(num_1)
+    result_twos_complement=bin_add(result,1)
+    return  result_twos_complement
+    
+
+
+def subtraction(num_1,num_2): # subtraction methode
+    result = int(num_2 ,2) - int(num_1 ,2)
+    sub = bin(result)
+    return sub[2:]
+    
+def bin_subtract(num_1,num_2): # to make subtraction operation always positive
+    if num_2 > num_1:
+        sub=subtraction(num_1,num_2)
     else:
-       print("Please insert a valid binary number.")
-    num_2 = input ("Enter the second number")
-    sub = num_2 - num_1
+        sub=subtraction( num_2,num_1)
     return sub
 
-def perform_action(choice_2):
-    if choice_2 == 'A':
-        result_1 = one_complement()
-        display_menu_1
+def perform_action_menu_2(choice,num_1):
+    if choice == 'A':
+         result_one_complement=one_complement(num_1)
+         print(f'Result of ones complement = {result_one_complement}\n')
     elif choice == 'B':
-        result_2 = two_complement()
-        display_menu_1
+          result_two_complement=two_complement(num_1)
+          print(f'Result of twos complement = {result_two_complement}\n')
     elif choice == 'C':
-         sum = addition()
-        display_menu_1()
+         num_2=insert_number()
+         sum=bin_add(num_1,num_2)
+         print(f'Sum = {sum}\n')
     elif choice == 'D':
-         sub = subtraction()
-        display_menu_1()
+         num_2=insert_number()
+         sub=bin_subtract(num_1,num_2)
+         print(f'Subtraction = {sub}\n')
+         
+    main()
         
-def perform_action(choice_1):
-    if choice_1 == 'A':
-        num_1 = input("Please Enter a number:")
-        validate_binary_number()
-        #understand it well
-        if validate_binary_number(num_1):
-            print("Valid binary number.")
-        else:
-            print("Please insert a valid binary number.")
+def perform_action_menu_1(choice):
+    if choice == 'A':
+        num_1 = input("Please Enter First Number:\n")
+        while not validate_binary_number(num_1):
+            print("Please insert a valid binary number.\n")
+            num_1 = input("Please Enter First Number Again\n")
         display_menu_2()
-        perform_action(choice_2)
-    elif choice_1 == 'B':
-         break
+        choice=get_user_choice()
+        perform_action_menu_2(choice,num_1)
+    elif choice == 'B':
+         quit()
     else:
-        print("Please select a valid choice")
+        print("Please select a valid choice(A\B) \n")
+        choice=get_user_choice()
 
-display_menu_1()
-get_user_choice_1()
+
+def main():
+    display_menu_1()
+    choice=get_user_choice()
+    perform_action_menu_1(choice)
     
-        
-    
-    
-    
+main()       
